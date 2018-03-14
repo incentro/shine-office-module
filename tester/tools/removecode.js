a0a815b99aea4604c2e4c2ef311ba8976baf9a68
Kasper Reijnders <kasper.reijnders@incentro.com>
2018-03-14 13:24:02 +0100

    fix removal of redundant code

--- tester/tools/removecode.js
+++ tester/tools/removecode.js
@@ -2,3 +2,6 @@ const del = require('del');
 del(['dist/!(*.umd.js|*.esm.js|*.d.ts|*.umd.js.map|*.esm.js.map|*.metadata.json)']).then(paths => {
   console.log('Files and folders that would be deleted:\n',paths.join('\n'));
 });
+del(['src/*.ngsummary.json', 'src/*.ngfactory.ts']).then(paths => {
+  console.log('Files and folders that would be deleted:\n',paths.join('\n'));
+});
