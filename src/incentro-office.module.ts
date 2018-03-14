ddc884d4eb2fe52d39f51f00872a8fad5b6365e1
Kasper Reijnders <kasper.reijnders@incentro.com>
2018-03-14 13:51:29 +0100

    updated appdinges

--- src/incentro-office.module.ts
+++ src/incentro-office.module.ts
@@ -1,5 +1,7 @@
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import {OfficeActionsService} from './office-actions.service';
+import {SampleModule} from "../newtester/src";
+import {SampleService} from "../newtester/src/sample.service";
 
 @NgModule({
   imports: [],
@@ -8,4 +10,10 @@ import {OfficeActionsService} from './office-actions.service';
   providers: [OfficeActionsService],
 })
 export class IncentroOfficeModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: IncentroOfficeModule,
+      providers: [OfficeActionsService]
+    };
+  }
 }
